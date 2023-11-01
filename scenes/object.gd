0b0a76e0b4227f7aafbfb4fd3292ff4dbe431ead
lucasfamelli <lucasfamelli@users.noreply.github.com>
2023-11-01 10:18:27 -0300

    script unified for player and asteroid

--- scenes/object.gd
+++ scenes/object.gd
@@ -1,8 +1,18 @@
 extends RigidBody2D
 
-@export var move_speed: int = 350
-@export var max_speed: int = 300
-@export var offset = 30
+@export var type: String
+@export var size: String = "large":
+	set(value):
+		if "large" in size:
+			offset = 60
+		if "medium" in size:
+			offset = 48
+		if "small" in size:
+			offset = 32
+
+var move_speed: int = 350
+var max_speed: int = 300
+var offset: int = 48
 
 var thrust = Vector2(move_speed,0)
 var torque = 50000 # 132 deg/s
@@ -11,6 +21,27 @@ var can_fire: bool = true
 
 var vert: bool = false
 var hori: bool = false
+
+func load_asteroid_texture():
+	var folder = DirAccess.open("res://graphics/asteroids/")
+	var possible_textures = Array()
+	if folder:
+		folder.list_dir_begin()
+		var file_name = folder.get_next()
+		while file_name != "":
+			if size in file_name and "import" not in file_name:
+				possible_textures.append(load("res://graphics/asteroids/" + file_name))
+			file_name = folder.get_next()
+	$AsteroidSprite.texture = possible_textures.pick_random()
+
+func adjust_asteroid_collision_size():
+	if size == "medium":
+		offset = 30
+		$CollisionShape2D.scale = Vector2(0.5, 0.5)
+	
+	if size == "small":
+		offset = 30
+		$CollisionShape2D.scale = Vector2(0.3, 0.3)
 
 func prepare_teleport(way):
 	if way == "v":
@@ -36,15 +67,8 @@ func teleport(state: PhysicsDirectBodyState2D):
 		hori = false
 	
 	state.set_transform(tar)
-		
-func _ready():
-	$CPUParticles2D.set_emitting(false)
-	$AnimatedSprite2D.play("idle")
 
-func _process(_delta):
-	pass
-
-func _integrate_forces(state):
+func move_player(state: PhysicsDirectBodyState2D):
 	if Input.is_action_pressed("up"):
 		state.apply_force(thrust.rotated(rotation))
 		$AnimatedSprite2D.play("moving")
@@ -66,6 +90,21 @@ func _integrate_forces(state):
 		var new_speed: Vector2 = state.get_linear_velocity().normalized()
 		new_speed *= max_speed
 		state.set_linear_velocity(new_speed)
+
+func _ready():
+	if type == "player":
+		$CPUParticles2D.set_emitting(false)
+		$AnimatedSprite2D.play("idle")
+	if "asteroid" in type:
+		load_asteroid_texture()
+		adjust_asteroid_collision_size()
+	
+func _process(_delta):
+	pass
+
+func _integrate_forces(state):
+	if type == "player":
+		move_player(state)
 	
 	if vert or hori:
 		teleport(state)
--- scenes/space_level.gd
+++ scenes/space_level.gd
@@ -1,21 +1,21 @@
 extends Node2D
 
 func _on_up_limit_body_entered(body):
-	print("up limit entered")
+	print("up limit entered by ", body)
 	if "prepare_teleport" in body:
 		body.prepare_teleport("v")
 
 func _on_down_limit_body_entered(body):
-	print("down limit entered")
+	print("down limit entered", body)
 	if "prepare_teleport" in body:
 		body.prepare_teleport("v")
 
 func _on_left_limit_body_entered(body):
-	print("left limit entered")
+	print("left limit entered", body)
 	if "prepare_teleport" in body:
 		body.prepare_teleport("h")
 
 func _on_right_limit_body_entered(body):
-	print("right limit entered")
+	print("right limit entered", body)
 	if "prepare_teleport" in body:
 		body.prepare_teleport("h")
